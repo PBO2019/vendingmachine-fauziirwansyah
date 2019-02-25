0365bc6c09fe86167087b53a4a4d965144bd03d8
Fauzi Irwansyah <46665519+fauziirwansyah@users.noreply.github.com>
2019-02-25 14:49:50 +0700

    Tugas
    Mesin Kopi dan Topping

---
+++ MesinKopi.java
@@ -0,0 +1,25 @@
+public class MesinKopi {
+    private String JenisKopi;
+    private String Topping;
+    private int JumlahStokKopi;
+
+    public void setJenisKopi(String jenisKopi) {
+        JenisKopi = jenisKopi;
+    }
+
+    public String getJenisKopi(){
+        return JenisKopi;
+    }
+    public void setJumlahStokKopi(int jumlahStokKopi){
+        JumlahStokKopi=jumlahStokKopi;
+    }
+    public int getJumlahStokKopi(){
+        return JumlahStokKopi;
+    }
+    public void setTopping(String toping){
+        Topping=toping;
+    }
+    public String getTopping(){
+        return Topping;
+    }
+}
